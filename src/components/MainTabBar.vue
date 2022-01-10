20be66b83c963e9175869385e331b0eeabdf7eb3
MoonEighteen <1911834200@qq.com>
2022-01-10 10:05:15 +0800

    delete something:

--- src/components/MainTabBar.vue
+++ src/components/MainTabBar.vue
@@ -2,26 +2,26 @@
  <div>
       <tab-bar>
       <tab-bar-item path="home" activeColor="red">
-       <template v-slot:item-icon><img src="@/assets/img/icons/home-11.svg" alt=""></template>
-       <template v-slot:item-icon-active><img src="@/assets/img/icons/town-15.svg" alt=""></template>
+       <template v-slot:item-icon><img src="@/assets/img/icons/airport.svg" alt=""></template>
+       <template v-slot:item-icon-active><img src="@/assets/img/icons/bakery.svg" alt=""></template>
        <template v-slot:item-text ><div>首页</div></template>
       </tab-bar-item>
 
       <tab-bar-item  path="category" activeColor="blue">
-        <template v-slot:item-icon><img src="@/assets/img/icons/heart-11.svg" alt=""> </template>
-        <template v-slot:item-icon-active><img src="@/assets/img/icons/star-15.svg" alt=""></template>
+        <template v-slot:item-icon><img src="@/assets/img/icons/bank.svg" alt=""> </template>
+        <template v-slot:item-icon-active><img src="@/assets/img/icons/bar.svg" alt=""></template>
         <template v-slot:item-text ><div>分类</div></template>
       </tab-bar-item>
       
       <tab-bar-item  path="cart" activeColor="green">
-        <template v-slot:item-icon><img src="@/assets/img/icons/post-11.svg" alt=""></template>
-        <template v-slot:item-icon-active><img src="@/assets/img/icons/shop-15.svg" alt=""></template>
+        <template v-slot:item-icon><img src="@/assets/img/icons/barrier.svg" alt=""></template>
+        <template v-slot:item-icon-active><img src="@/assets/img/icons/basketball.svg" alt=""></template>
         <template v-slot:item-text ><div>购物车</div></template>
       </tab-bar-item>
 
       <tab-bar-item  path="profile">
-        <template v-slot:item-icon><img src="@/assets/img/icons/volcano-11.svg" alt=""></template>
-        <template v-slot:item-icon-active><img src="@/assets/img/icons/rocket-15.svg" alt=""></template>
+        <template v-slot:item-icon><img src="@/assets/img/icons/bus.svg" alt=""></template>
+        <template v-slot:item-icon-active><img src="@/assets/img/icons/cafe.svg" alt=""></template>
         <template v-slot:item-text ><div>我的</div></template>
       </tab-bar-item>
     </tab-bar>
